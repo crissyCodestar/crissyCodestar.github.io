564e7c14c942fb3a3a33027244b703b92d19928e
Crissy <crystalgrant82@gmail.comgit config --global user.email crystalgrant82@gmail.com>
2019-04-03 18:41:36 -0400

    Updates

---
+++ precache-manifest.0fa73b9e396e39c3cb98c98774d46422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca0e24f9c9053dc2d849",
+    "url": "/static/js/main.a1518354.chunk.js"
+  },
+  {
+    "revision": "f850218a9fcb039e8f2d",
+    "url": "/static/js/2.5265c3af.chunk.js"
+  },
+  {
+    "revision": "ca0e24f9c9053dc2d849",
+    "url": "/static/css/main.44147cc6.chunk.css"
+  },
+  {
+    "revision": "563272e62e84b597ea87568a9f591aef",
+    "url": "/index.html"
+  }
+];
